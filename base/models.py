e9928d3a544fc43bcace11ae7506acdfa5cf45d8
odiegofarias <diego.farias.fernandes@gmail.com>
2022-12-12 20:19:36 -0300

    Criando uma Manager de usuários para trabalhar com a criação de usuários e superusuários com algumas modificações. Como os campos não são iguais ao AbstraticUser padrão, herdamos de BaseUserManager

--- base/models.py
+++ base/models.py
@@ -1,8 +1,45 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 from django.core.mail import send_mail
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+            Cria e salva usuários com email e senha fornecidos
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+
+    # Usuário criados não vêm como superusuários
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+    
+    # Cria super usuários com email, senha e os campos adicionais
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True. ')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must jave is_staff=True. ')
+
+
+        return self._create_user(email, password, **extra_fields)
 
 
 # Create your models here.
@@ -29,7 +66,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     )
     date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
 
-    # objects = UserManager()
+    objects = UserManager()
 
     EMAIL_FIELD = "email"
     USERNAME_FIELD = "email"
